8bc0b36d09d335b9f1689d2a376b787c18cd3d4c
Dack <dacka@mskcc.org>
2018-02-16 22:36:19 -0500

    Add lodash

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 import fs from "fs";
 import puppeteer from "puppeteer";
-
-import { BEERS_URL, BEER_IMAGE_TAG } from "./utils";
+import zipWith from "lodash/zipWith";
+import {
+  BEERS_URL,
+  BEER_IMAGE_TAG,
+  BEER_NAME,
+  BEER_TYPE,
+  BEER_SHORT_DESCRIPTION
+} from "./utils";
 
 async function extractBeerImages() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await page.goto(BEERS_URL);
-
   const imageHandler = await page.$$(BEER_IMAGE_TAG);
+
   imageHandler.forEach(async imageTag => {
     const link = await page.evaluate(sel => sel.currentSrc, imageTag);
     axios({
@@ -27,4 +33,34 @@ async function extractBeerImages() {
   await page.close();
 }
 
-extractBeerImages();
+async function extractBeerMetadata(tag) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(BEERS_URL);
+  const beerNameHandler = await page.$$(tag);
+
+  return await Promise.all(
+    beerNameHandler.map(async name => {
+      try {
+        return await page.evaluate(sel => sel.innerHTML, name);
+      } catch (error) {
+        throw error;
+      }
+    })
+  );
+}
+
+async function fetchAllMetadata() {
+  const beerNames = await extractBeerMetadata(BEER_NAME);
+  const beerTypes = await extractBeerMetadata(BEER_TYPE);
+
+  return zipWith(beerNames, beerTypes, (name, type) => {
+    return {
+      name,
+      type
+    };
+  });
+}
+
+fetchAllMetadata();
--- utils.js
+++ utils.js
@@ -1,2 +1,6 @@
 export const BEERS_URL = "http://jesterkingbrewery.com/beers/";
 export const BEER_IMAGE_TAG = "img.center";
+
+export const BEER_NAME = "dt > h2 > strong";
+export const BEER_TYPE = "dd > h2 > i";
+export const BEER_SHORT_DESCRIPTION = "dd > p";
